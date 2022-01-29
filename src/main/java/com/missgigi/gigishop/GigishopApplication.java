ea000fc8756b8eda1d82cdb8b1e5664647037354
rafiks <rab@devsphoto.com>
2022-01-29 10:40:33 -0500

    Initial commit

---
+++ src/main/java/com/missgigi/gigishop/GigishopApplication.java
@@ -0,0 +1,13 @@
+package com.missgigi.gigishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigishopApplication.class, args);
+	}
+
+}
