643128ba9df41d578884edeb21c1477cb219e627
rafiks <rab@devsphoto.com>
2022-01-29 20:07:19 -0500

    backend complete

---
+++ src/main/java/com/missgigi/gigishop/controller/ReservationResource.java
@@ -0,0 +1,34 @@
+package com.missgigi.gigishop.controller;
+
+import com.missgigi.gigishop.model.Reservation;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping(ReservationResource.ROOM_V_1_RESERVATION)
+@CrossOrigin
+public class ReservationResource {
+    public static final String ROOM_V_1_RESERVATION = "/room/v1/reservation/";
+
+    @GetMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> getReservationById(@PathVariable String roomId){
+        // reservationService.getReservation(roomId);
+       return Mono.just("{}");
+    }
+    @PostMapping(path="", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> createReservation(@RequestBody Mono<Reservation> reservation){
+        return Mono.just("{}");
+
+    }
+    @PutMapping(path="{roomId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> updatePrice(@PathVariable String roomId, @RequestBody Mono<Reservation> reservation){
+        return Mono.just("{}");
+    }
+    @DeleteMapping(path="{roomId}")
+    public Mono<Boolean> deleteReservation(@PathVariable String roomId){
+        return Mono.just(true);
+    }
+}
